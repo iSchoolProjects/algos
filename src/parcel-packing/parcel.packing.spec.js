ba11170b89d8deaa026421987edcbb92d5aa46e7
zlajabekrija <zlaja.bekrija@gmail.com>
2022-10-19 10:06:04 +0200

    update_test

--- src/parcel-packing/parcel.packing.spec.js
+++ src/parcel-packing/parcel.packing.spec.js
@@ -12,7 +12,7 @@ test('check for null for over weight', () => {
 test('check for null for over size', () => {
   expect(
     parcelPacking(270, 20, [
-      { height: 40, width: 30, length: 30, weight: 25, total: 3 },
+      { height: 40, width: 30, length: 30, weight: 5, total: 3 },
       { height: 130, width: 80, length: 10, weight: 3, total: 3 },
     ])
   ).toBe(null);
