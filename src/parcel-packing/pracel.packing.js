ecb4dc184fec1b57cc52fd8f02678043404f8ebd
zlajabekrija <zlaja.bekrija@gmail.com>
2022-10-21 00:12:09 +0200

    remove_unrelated

--- src/parcel-packing/pracel.packing.js
+++ src/parcel-packing/pracel.packing.js
@@ -4,42 +4,6 @@ export default function parcelPacking(
   maxSideLenght,
   items
 ) {
-  const [width, height, length] = [
-    items.length,
-    items.height,
-    items.width,
-  ].sort((a, b) => b - a);
-  const itemBeltSize = beltSize(width, height, length);
-  if (
-    itemBeltSize > maxBeltSize ||
-    items.weight > maxWeight ||
-    width > maxSideLenght
-  )
-    return null;
-
-  let total = 0;
-  const itemsPerPackage = [];
-  while (items.total > 0) {
-    let currentBeltSize = itemBeltSize;
-    let itemsInPackage = 0;
-    let currentWeight = items.weight;
-    while (
-      currentWeight <= maxWeight &&
-      currentBeltSize <= maxBeltSize &&
-      items.total > 0
-    ) {
-      currentWeight += items.weight;
-      currentBeltSize += length;
-      itemsInPackage++;
-      items.total--;
-    }
-    itemsPerPackage.push(itemsInPackage);
-    total++;
-  }
-  return {
-    total,
-    itemsPerPackage,
-  };
   // please fill out the rest :)
 }
 
